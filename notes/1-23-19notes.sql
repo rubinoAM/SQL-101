05e846f4f9856570de9379b6e223ff1c80122a9d
rubinoAM <parkcathedralgrace@gmail.com>
2019-01-23 10:28:26 -0500

    LIKE

--- notes/1-23-19notes.sql
+++ notes/1-23-19notes.sql
@@ -20,3 +20,11 @@ SELECT * FROM orders
 	WHERE status = 'Cancelled' OR status = 'Resolved';
 SELECT * FROM orders
 	WHERE orderDate < "2004-01-01" AND orderDate > "2003-01-01";
+	
+-- LIKE is basically an equal sign
+-- The big advantage here is that you can use % (wildcard) in your string
+SELECT * FROM productlines
+	WHERE productLine LIKE 'planes';
+-- We want last names with 'ur' in them, and first names that start with 'd'
+SELECT * FROM employees
+	WHERE lastName LIKE '%ur%' AND firstName LIKE 'D%';

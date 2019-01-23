6af7a607ee9edc6dc8748ca53c56701482acb73f
rubinoAM <parkcathedralgrace@gmail.com>
2019-01-23 15:14:36 -0500

    Homework

---
+++ restaurant_db/restaurant.sql
@@ -0,0 +1,10 @@
+CREATE TABLE restaurant(
+	id int(11) NOT NULL AUTO_INCREMENT,
+	name varchar(50),
+	distance float(4,2),
+	stars int(5),
+	category varchar(50),
+	favoriteDish varchar(50),
+	doesTakeout int(1),
+	lastTimeYouAteThere date
+);
---
+++ restaurant_db/restaurant_insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO restaurant (name,distance,stars,category,favoriteDish,doesTakeout,lastTimeYouAteThere)
+	VALUES('Lovies BBQ',0.1,4,'Barbecue','Baby-Back Ribs',0,'2018-12-02'),
+	('Chipotle Mexican Grill',0.03,3,'Mexican','Chicken Quesadilla',1,'2018-12-16'),
+	('Naanstop',0.1,4,'Indian','Chicken Tikka Massala',0,'2018-12-13'),
+	('Farm Burger',0.1,5,'Hamburgers','Spicy Chicken Sandwich',0,'2019-01-10'),
+	('Chama Gaúcha Brazilian Steakhouse',0.2,5,'Brazilian','Picanha',0,'2018-09-12'),
+	('Fogo de Chão Brazilian Steakhouse',0.8,4,'Brazilian','Filet Mignon',0,'2018-10-31'),
+	('Bistro Niko',0.5,4,'French','Foie Gras',0,'2018-11-02');

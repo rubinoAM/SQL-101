0428bb2a1dbf27fb40f6107f6258698d96617a7c
rubinoAM <parkcathedralgrace@gmail.com>
2019-01-23 10:22:07 -0500

    DISTINCT and WHERE clauses

--- notes/1-23-19notes.sql
+++ notes/1-23-19notes.sql
@@ -6,8 +6,17 @@
 	After the column(s) comes the table you want to query
     Always end SQL statements with a ';'*/
 SELECT * FROM offices;
-SELECT firstName, lastName from employees;
+SELECT firstName, lastName FROM employees;
 
 -- If you want only certain rows, add WHERE after the table
-SELECT * FROM employees
+SELECT firstName, lastName, extension FROM employees
 	WHERE jobTitle = "Sales Rep";
+	
+-- We can ask for distinct results
+SELECT DISTINCT(jobTitle) FROM employees;
+
+-- WHERE clauses can have multiple conditions using AND + OR (Use parentheses)
+SELECT * FROM orders
+	WHERE status = 'Cancelled' OR status = 'Resolved';
+SELECT * FROM orders
+	WHERE orderDate < "2004-01-01" AND orderDate > "2003-01-01";

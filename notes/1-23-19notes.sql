662942dd2dfd836a182eb4949de8c75553330e4d
rubinoAM <parkcathedralgrace@gmail.com>
2019-01-23 09:59:53 -0500

    First commit

---
+++ notes/1-23-19notes.sql
@@ -0,0 +1,8 @@
+SELECT * FROM offices;
+/*  SELECT = Grabs records/rows
+	After SELECT comes what columns you want
+	Options: * = every available column
+			 city = only the city column
+			 city, phone = city & phone columns
+	After the column(s) comes the table you want to query
+    Always end SQL statements with a ';'*/

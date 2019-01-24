087b73b5550c948eef0dac700a76e27a93af7525
rubinoAM <parkcathedralgrace@gmail.com>
2019-01-24 11:39:50 -0500

    Troubleshooting node

--- todoMysql/routes/index.js
+++ todoMysql/routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+//Use mysql module to connect & query from express/node
+//This module isn't part of core, so npm install it
+var mysql = require('mysql');
+var config = require('../config')
+var connection = mysql.createConnection(config);
+connection.connect();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  /*  We want to load a list of our restaurants on the homepage
+      This is inside of mysql
+      Before we render the view, we want to get the data and send it over to the view*/
   res.render('index', { title: 'Express' });
 });
 

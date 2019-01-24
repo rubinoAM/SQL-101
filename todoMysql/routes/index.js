a6f3177e23a14e48cf1461d4dc7fabcb213c6eec
rubinoAM <parkcathedralgrace@gmail.com>
2019-01-24 14:08:47 -0500

    HTML Form adds to DB

--- todoMysql/routes/index.js
+++ todoMysql/routes/index.js
@@ -10,10 +10,28 @@ connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  /*  We want to load a list of our restaurants on the homepage
-      This is inside of mysql
-      Before we render the view, we want to get the data and send it over to the view*/
   res.render('index', { title: 'Express' });
+});
+
+router.post('/addItem',(req, res, next)=>{
+  const newTask = req.body.newTask;
+  const newTaskDate = req.body.newTaskDate;
+  //We know what the user submitted, it comes in req.body.NAMEOFFIELD
+  //We store those values into var's and insert them into the SQL
+
+  //const badInsertQuery = `INSERT INTO task (taskName, taskDate) VALUES (${newTask},${newTaskDate});`;
+  // ^ : Susceptible to SQL Injections
+  const insertQuery = `INSERT into tasks (taskName,taskDate) VALUES (?,?);`;
+  connection.query(insertQuery,[newTask,newTaskDate],(err,results)=>{
+    if(err){
+      throw(err);
+    } else {
+      //QUERY SUCCESS! Keep the user on the homepage!
+      res.redirect('/');
+    }
+    
+  });
+  //res.json(req.body);
 });
 
 module.exports = router;

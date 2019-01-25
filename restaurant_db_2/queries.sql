e44c18c693bfcd4a6a3a0beee987e32a35f2b8d5
rubinoAM <parkcathedralgrace@gmail.com>
2019-01-25 13:50:48 -0500

    Query 4

--- restaurant_db_2/queries.sql
+++ restaurant_db_2/queries.sql
@@ -6,6 +6,10 @@ SELECT users.name,AVG(reviews.stars) AS average_rating FROM users
 	INNER JOIN reviews ON users.id = reviews.author_id
 	GROUP BY users.id;
 
-SELECT MIN(reviews.stars) AS lowest_rating,users.name FROM reviews
+SELECT users.name,MIN(reviews.stars) AS lowest_rating FROM reviews
 	INNER JOIN users on users.id = reviews.author_id
 	GROUP BY users.id;
+
+SELECT restaurants.name, COUNT(reviews.id) AS num_reviews FROM reviews
+	INNER JOIN restaurants on restaurants.id = reviews.restaurant_id
+	GROUP BY restaurants.id;
